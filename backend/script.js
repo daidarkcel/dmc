44451c7ca483dc7b6e55df84f58714ba16cb640d
daidarkcel <fountainnessv@gmail.com>
2025-03-20 02:28:44 -0400

    daidarkcel welcomes knowledge. version v0.5.2

--- backend/script.js
+++ backend/script.js
@@ -1,3 +1,55 @@
-function boneConversion()
-const bones = document.getElementById('bones').value;
-const rate = bones * ;
+function boneConversion() {
+	// function body
+    // when the user tries to use the converter to convert their us dollars into bones
+
+}
+
+function jukeboxSummon() {
+	// function body
+    // summons the jukebox
+}
+
+function easterEgg() {
+	// function body
+    // when user types in 'ketamine'', the site logo will change to it's alternate logo, and a song unlocks in the jukebox.
+    // remains in use until user leaves the site.
+
+}
+
+function siteEntrance() {
+    // function body
+    // when the user tries to enter the site by clicking the bone.
+    // store how many times it has been clicked by a 
+
+}
+
+
+
+
+
+//┓┏┏┓┳┓┏┓┳┏┓┳┓
+//┃┃┣ ┣┫┗┓┃┃┃┃┃
+//┗┛┗┛┛┗┗┛┻┗┛┛┗
+//┏┓ ┏┓ ┏┓
+//┏┛ ┃┫ ┃┫
+//┗━•┗┛•┗┛
+          
+
+function adminFail() {
+    // function body
+    // when the user tries to login to the admin panel and fails
+
+}
+
+function adminLogin() {
+    // function body
+    // when the user tries to login 
+
+}
+
+
+function adminLogout() {
+    // function body
+    // when the user tries to logout from the admin panel
+
+}
