2ee53ed0b3bb41b0b56e359be35f38fd5d3ca524
daidarkcel <fountainnessv@gmail.com>
2025-03-23 03:45:19 -0400

    changed alot, get ready for the 31st
    (version 1 will be there)

--- backend/script.js
+++ backend/script.js
@@ -1,3 +1,29 @@
+//┳┓┏┓┏┳┓┏┓
+//┣┫┣  ┃ ┣┫
+//┻┛┗┛ ┻ ┛┗
+         
+
+function siteEntrance() {
+    // function body
+    // when the user tries to enter the site by clicking the bone.
+    // store how many times it has been clicked by a 
+
+}
+
+function easterEgg() {
+	// function body
+    // when user types in 'ketamine'', the site logo will change to it's alternate logo, and a song unlocks in the jukebox.
+    // remains in use until user leaves the site.
+
+}
+
+
+//┓┏┏┓┳┓┏┓┳┏┓┳┓  ┓ ┏┓ ┏┓
+//┃┃┣ ┣┫┗┓┃┃┃┃┃━━┃ ┃┫ ┃┫
+//┗┛┗┛┛┗┗┛┻┗┛┛┗  ┻•┗┛•┗┛
+                      
+
+
 function boneConversion() {
 	// function body
     // when the user tries to use the converter to convert their us dollars into bones
@@ -9,21 +35,11 @@ function jukeboxSummon() {
     // summons the jukebox
 }
 
-function easterEgg() {
-	// function body
-    // when user types in 'ketamine'', the site logo will change to it's alternate logo, and a song unlocks in the jukebox.
-    // remains in use until user leaves the site.
+function emailSent()
+{
+
 
 }
-
-function siteEntrance() {
-    // function body
-    // when the user tries to enter the site by clicking the bone.
-    // store how many times it has been clicked by a 
-
-}
-
-
 
 
 
@@ -34,6 +50,8 @@ function siteEntrance() {
 //┏┛ ┃┫ ┃┫
 //┗━•┗┛•┗┛
           
+
+// expect to get to this part around april. 
 
 function adminFail() {
     // function body
@@ -53,3 +71,29 @@ function adminLogout() {
     // when the user tries to logout from the admin panel
 
 }
+
+
+function adminSession() {   
+//function body
+// when the site recognizes the user is an admin, so gives them the buttons to access admin panel when online
+// if user is not an admin or a user and is just a visitor, ignore the existence of this function entirely.
+
+}
+
+
+
+function versionUpdate() {
+    // function body
+    // automatically update the version based on what is typed in the admin panel
+    // for ex: if admin panel reports update, user has to verify changes, then type in version number
+    //
+    
+    
+    }
+    
+    function updateReport() {
+// function body
+    // when a commit is updated, capture what was changed/added in @#version and post it to admin panel.html
+    // do not allow 'function versionUpdate' to run unless these changes are verified
+
+    }
