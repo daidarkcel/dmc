03fd935c1f336da67c30e08bfa4e3e6ba7aa3c8e
daidarkcel <fountainnessv@gmail.com>
2025-12-19 09:57:32 -0500

    the last push before 2025 is over haha!!!

---
+++ backend/preload.js
@@ -0,0 +1,5 @@
+// this file is for things that will load when the site is loaded. this is for loading things like fonts and media that'll be stored in the database.
+
+
+document.addEventListener('DOMContentLoaded')
+document.querySelector('.close-modal').removeEventListener = 'none';
---
+++ backend/scripts.js
@@ -0,0 +1,148 @@
+
+//┓┏┏┓┳┓┏┓┳┏┓┳┓  ┓ ┏┓ ┏┓
+//┃┃┣ ┣┫┗┓┃┃┃┃┃━━┃ ┃┫ ┃┫
+//┗┛┗┛┛┗┗┛┻┗┛┛┗  ┻•┗┛•┗┛
+
+
+
+
+
+
+
+
+function siteEntrance() {
+    // function body
+    // when the user tries to enter the site by clicking the bone.
+    // store how many times it has been clicked.
+
+}
+
+function easterEgg() {
+	// function body
+    // when user types in 'korsadia', the site logo will change to it's alternate logo, and a song unlocks in the jukebox.
+    // remains in use until user leaves the site.
+
+}
+
+                     
+function cursorSelection() {
+    // function body
+    // when the user decides which cursor they want to use to navigate the site
+
+}
+
+function boneConversion() {
+	// function body
+    // when the user tries to use the converter to convert their us dollars into bones
+
+}
+
+function jukeboxSummon() {
+	// function body
+    // summons the jukebox
+}
+
+function emailSent() {
+// when an email gets sent to me from the credits page submission
+// should notify the user whether the email was successful or not, if fail ask to try again.
+// if successful deny the user from sending the email again by storing this info in a cookie.
+
+
+}
+
+function testTitlechange() {}
+
+// when the admin enters the test page, have a different title everytime they enter the page.
+// this is for fucking fun lol
+// have it be a random title from a list of 30 titles.
+// have it change everytime the page is refreshed.
+
+
+function nyanDeath() {
+// when the user enters the legacy page, have a random nyan cat death sound play and then have the WIP message appear.
+
+
+
+}
+
+
+function blogAccess() {}
+// when the user tries to enter the blog page via the blog button on the credits page, have a WIP sticker appear over the button. if the button is clicked 5 times, direct the user to the page.
+
+
+
+
+//┓┏┏┓┳┓┏┓┳┏┓┳┓
+//┃┃┣ ┣┫┗┓┃┃┃┃┃
+//┗┛┗┛┛┗┗┛┻┗┛┛┗
+//┏┓ ┏┓ ┏┓
+//┏┛ ┃┫ ┃┫
+//┗━•┗┛•┗┛
+          
+
+// expect to get to this part around mid - april. 
+
+function adminFail() {
+    // function body
+    // when the user tries to login to the admin panel and fails
+
+}
+
+function adminLogin() {
+    // function body
+    // when the user tries to login 
+
+}
+
+
+function adminLogout() {
+    // function body
+    // when the user tries to logout from the admin panel
+
+}
+
+
+function adminSession() {   
+//function body
+// when the site recognizes the user is an admin, so gives them the buttons to access admin panel when online
+// if user is not an admin or a user and is just a visitor, ignore the existence of this function entirely.
+
+}
+
+
+
+function versionUpdate() {
+    // function body
+    // automatically update the version based on what is typed in the admin panel
+    // for ex: if admin panel reports update, user has to verify changes, then type in version number
+    //
+    
+    
+    }
+    
+    function updateReport() {
+    // function body
+    // when a commit is updated, capture what was changed/added in @#version and post it to admin panel.html
+    // do not allow 'function versionUpdate' to run unless these changes are verified
+    // have it read the last 20 lines of the @version file and relay it towards a panel in MOJA.
+    }
+
+    function inquiryOpen() {
+    // function body
+    // when the user presses the question mark on the portfolio page, the modal about the page is displayed.
+    // when the user presses the x button, the modal closes.
+    
+        document.querySelector('.inquiry-modal').style.display = 'block';
+        
+        
+    }
+        
+
+    function inquiryClose() {
+        // function body
+        // when the user presses the x button, the modal closes.
+        document.querySelector('.inquiry-modal').style.display = 'none';
+    }
+
+
+    document
